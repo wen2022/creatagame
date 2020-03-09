ba452e9a5c77bcfcebbd69109787daf8f5b50357
wen chung <t107590014@ntut.org.tw>
2020-03-09 16:28:35 +0800

    test3

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
-	return false;
 }
 int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
+		std::cout << "Choose a number between 1 - 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
